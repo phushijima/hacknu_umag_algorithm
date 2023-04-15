9ae3a2849b60c97543c57cea5acf227d1c30c34c
phushijima <71209484+phushijima@users.noreply.github.com>
2023-04-16 03:35:46 +0600

    Update algon.py
    step 2/6
    before optimization
    without CRUD

--- algon.py
+++ algon.py
@@ -5,16 +5,33 @@
 
 # Algorithm for identifying m_1 and y_1 
 def sales_before_time(sales, fromTime):
-    # Summing up quantities of m_i until saleTime >= fromTime, and taking the last sale element as y_1
+    # Sums up quantities of m_i until saleTime >= fromTime, and taking the last sale element as y_1
     # INPUT: sales <list>, fromTime <string>
-    # OUTPUT: m_sum_1 <int>, y_1_id <int>
+    # OUTPUT: m_sum_1 <int>, yFirst_id <int>
     i = 0
     m_sum_1 = 0
-    y_1_id = 0
+    yFirst_id = 0
 
     while sales[i]['saleTime'] < fromTime:
         m_sum_1 += sales[i]['quantity']
-        y_1_id = sales[i]['id']
+        yFirst_id = sales[i]['id']
 
-    return m_sum_1, y_1_id
+    return m_sum_1, yFirst_id
 
+def supplies_before_time(supplies, m_sum_1):
+    # Sums up quantities of n_i until n_sum_1 > m_sum_1
+    # Takes the element n_{i-1} (before the last) and calculates the difference delta_n, where sum(0, x_{i-1}) + delta_n = sum(0, y_1)
+    # INPUT: supplies <list>, m_sum_1 <int>
+    # OUTPUT: delta_n <int>, xFirst_id <int>
+
+    i = 0
+    n_sum_1 = 0
+    delta_n = 0
+    xFirst_id = 0
+
+    while n_sum_1 > m_sum_1:
+        xFirst_id = supplies[i]['id']
+        n_sum_1 += supplies[i]['quantity']
+    
+    delta_n = n_sum_1 - m_sum_1
+    return delta_n, xFirst_id
