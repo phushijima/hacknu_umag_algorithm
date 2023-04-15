68b6a847513c2f06d7ef3657e2c3d0ad83019c48
phushijima <71209484+phushijima@users.noreply.github.com>
2023-04-16 04:00:15 +0600

    Update algon.py
    step 3/6
    before optimization
    without CRUD

--- algon.py
+++ algon.py
@@ -4,21 +4,24 @@
 # n_1, n_2 - количества (quantity) принадлежащие определенным закупам y_1 и y_2 с которого начинается и заканчивается подсчет для cost
 
 # Algorithm for identifying m_1 and y_1 
-def sales_before_time(sales, fromTime):
+def sales_before_fromTime(sales, fromTime):
     # Sums up quantities of m_i until saleTime >= fromTime, and taking the last sale element as y_1
     # INPUT: sales <list>, fromTime <string>
-    # OUTPUT: m_sum_1 <int>, yFirst_id <int>
+    # OUTPUT: m_sum_1 <int>, yFirst_id <int>, yFirst_index <int>
     i = 0
     m_sum_1 = 0
     yFirst_id = 0
+    yFirst_index = 0
 
     while sales[i]['saleTime'] < fromTime:
         m_sum_1 += sales[i]['quantity']
         yFirst_id = sales[i]['id']
+        yFirst_index = i
+        i += 1
 
-    return m_sum_1, yFirst_id
+    return m_sum_1, yFirst_id, yFirst_index
 
-def supplies_before_time(supplies, m_sum_1):
+def supplies_before_fromTime(supplies, m_sum_1):
     # Sums up quantities of n_i until n_sum_1 > m_sum_1
     # Takes the element n_{i-1} (before the last) and calculates the difference delta_n, where sum(0, x_{i-1}) + delta_n = sum(0, y_1)
     # INPUT: supplies <list>, m_sum_1 <int>
@@ -32,6 +35,23 @@ def supplies_before_time(supplies, m_sum_1):
     while n_sum_1 > m_sum_1:
         xFirst_id = supplies[i]['id']
         n_sum_1 += supplies[i]['quantity']
+        i += 1
     
     delta_n = n_sum_1 - m_sum_1
     return delta_n, xFirst_id
+
+def sales_before_toTime(sales, toTime, m_sum_1, yFirst_index):
+    # Sums up quantities of m_i until saleTime <= toTime, and taking the last sale element as y_2
+    # INPUT: sales <list>, toTime <string>, m_sum_1 <int>, yFirst_index <int>
+    # OUTPUT: m_sum_2 <int>, ySecond_id <int>
+    
+    i = yFirst_index
+    m_sum_2 = m_sum_1
+    ySecond_id = 0
+
+    while sales[i]['saleTime'] < toTime:
+        m_sum_2 += sales[i]['quantity']
+        ySecond_id = sales[i]['id']
+        i += 1
+
+    return m_sum_2, ySecond_id
